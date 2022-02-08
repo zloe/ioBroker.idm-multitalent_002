3873a26e4e99ee8906c622956e5cdfb0b0d55a0d
Klaus Zloebl <klaus@zloebl.net>
2022-02-08 19:29:12 +0100

    initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'idm-multitalent_002 adapter settings': {
+        'en': 'Adapter settings for idm-multitalent_002',
+        'de': 'Adaptereinstellungen für idm-multitalent_002',
+        'ru': 'Настройки адаптера для idm-multitalent_002',
+        'pt': 'Configurações do adaptador para idm-multitalent_002',
+        'nl': 'Adapterinstellingen voor idm-multitalent_002',
+        'fr': "Paramètres d'adaptateur pour idm-multitalent_002",
+        'it': "Impostazioni dell'adattatore per idm-multitalent_002",
+        'es': 'Ajustes del adaptador para idm-multitalent_002',
+        'pl': 'Ustawienia adaptera dla idm-multitalent_002',
+        'zh-cn': 'idm-multitalent_002的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'option1',
+        'ru': 'option1',
+        'pt': 'option1',
+        'nl': 'option1',
+        'fr': 'option1',
+        'it': 'option1',
+        'es': 'option1',
+        'pl': 'option1',
+        'zh-cn': 'option1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'option2',
+        'ru': 'option2',
+        'pt': 'option2',
+        'nl': 'option2',
+        'fr': 'option2',
+        'it': 'option2',
+        'es': 'option2',
+        'pl': 'option2',
+        'zh-cn': 'option2'
+    }
+};
