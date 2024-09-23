d544222a5a468021ea4f89d136cb57460af38fcd
zloe <klaus@zloebl.net>
2024-09-23 17:06:02 +0200

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+export default [
+  {
+    root: true,
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    },
+    env: {
+      es2021: true,
+      node: true,
+      mocha: true
+    },
+    plugins: [],
+    rules: {
+      indent: ['error', 4, { SwitchCase: 1 }],
+      'no-console': 'off',
+      'no-var': 'error',
+      'no-trailing-spaces': 'error',
+      'prefer-const': 'error',
+      quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+      semi: ['error', 'always']
+    }
+  }
+];
