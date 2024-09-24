e3bf358e5396589144d08f3ab255650d98343032
zloe <klaus@zloebl.net>
2024-09-24 08:04:23 +0200

    Update eslint.config.mjs fixed spacing

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,32 +1,32 @@
 export default [
-  {
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      globals: {
-        // Mocha globals
-        describe: 'readonly',
-        it: 'readonly',
-        before: 'readonly',
-        after: 'readonly',
-        beforeEach: 'readonly',
-        afterEach: 'readonly',
+    {
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            globals: {
+                // Mocha globals
+                describe: 'readonly',
+                it: 'readonly',
+                before: 'readonly',
+                after: 'readonly',
+                beforeEach: 'readonly',
+                afterEach: 'readonly',
 
-        // Node.js globals
-        global: 'readonly',
-        process: 'readonly',
-        __dirname: 'readonly',
-        module: 'readonly'
-      }
-    },
-    rules: {
-      indent: ['error', 4, { SwitchCase: 1 }],
-      'no-console': 'off',
-      'no-var': 'error',
-      'no-trailing-spaces': 'error',
-      'prefer-const': 'error',
-      quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
-      semi: ['error', 'always']
+                // Node.js globals
+                global: 'readonly',
+                process: 'readonly',
+                __dirname: 'readonly',
+                module: 'readonly'
+            }
+        },
+        rules: {
+            indent: ['error', 4, { SwitchCase: 1 }],
+            'no-console': 'off',
+            'no-var': 'error',
+            'no-trailing-spaces': 'error',
+            'prefer-const': 'error',
+            quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+            semi: ['error', 'always']
+        }
     }
-  }
 ];
